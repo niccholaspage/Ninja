31a5e1f3defa8ec4de0e09ed7fac2b56c3fbdaf7
Nicholas <niccholaspage@gmail.com>
2016-04-28 22:24:25 -0400

    Use immersive mode on KitKat and up

--- android/src/com/nicholasnassar/ninja/AndroidLauncher.java
+++ android/src/com/nicholasnassar/ninja/AndroidLauncher.java
@@ -1,15 +1,15 @@
 package com.nicholasnassar.ninja;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new NinjaGame(null), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
+        initialize(new NinjaGame(null), config);
+    }
 }
